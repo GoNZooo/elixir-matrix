9e36fbe576e0da111f41d57d6f069feb1c80ce2d
Rickard Andersson <gonz@severnatazvezda.com>
2015-11-24 05:02:40 +0200

    Normalize atoms with punctuation and the like in them

--- lib/matrix/atomifier.ex
+++ lib/matrix/atomifier.ex
@@ -4,7 +4,11 @@ defmodule Matrix.Atomifier do
   """
 
   defp atom_key(x) when is_bitstring(x) do
-    String.to_atom(x)
+    x
+    # Atoms will have strange formatting with punctuation in them.
+    # Better to simply replace everything with the canonical underscore
+    |> String.replace(~r/[.:-]/, "_") 
+    |> String.to_atom
   end
 
   defp atom_val(x) when is_map(x) do
@@ -19,6 +23,18 @@ defmodule Matrix.Atomifier do
     x
   end
 
+  @doc"""
+  Recursively atomifies a JSON object as parsed by Poison.
+
+      iex> Matrix.Atomifier.atomify(%{"one" => [%{"two" => "three"}, [%{"four"=> "five"}]]})
+      %{one: [%{two: "three"}, [%{four: "five"}]]}
+
+      iex> Matrix.Atomifier.atomify(%{"one" => ["1", 1, 1.5], "two" => %{"three" => "four"}})
+      %{one: ["1", 1, 1.5], two: %{three: "four"}}
+
+      iex> Matrix.Atomifier.atomify(%{"m.room.message" => %{"what-e:v:e:r" => true}})
+      %{m_room_message: %{what_e_v_e_r: true}}
+  """
   def atomify(x) when is_map x do
     Enum.into(x, %{}, fn {key, value} -> {atom_key(key), atom_val(value)} end)
   end
